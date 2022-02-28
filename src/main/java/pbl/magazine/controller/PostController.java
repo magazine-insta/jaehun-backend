b674f90471f3445d825fb462dd2fa65e4e5a5c10
HoduUlmu <62408879+HoduUlmu@users.noreply.github.com>
2022-02-28 14:33:48 +0900

    JWT

--- src/main/java/pbl/magazine/controller/PostController.java
+++ src/main/java/pbl/magazine/controller/PostController.java
@@ -1,11 +1,12 @@
 package pbl.magazine.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import pbl.magazine.dto.PostRequestDto;
 import pbl.magazine.dto.PostResponseDto;
-import pbl.magazine.model.User;
 import pbl.magazine.security.UserDetailsImpl;
 import pbl.magazine.service.PostService;
 
@@ -18,13 +19,13 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/api/post")
-    public List<PostResponseDto> getListOfPosts() {
-        return postService.getListOfPost();
+    public ResponseEntity<List<PostResponseDto>> getListOfPosts() {
+        return ResponseEntity.ok().body(postService.getListOfPost());
     }
 
     @GetMapping("/api/post/{id}")
-    public PostResponseDto getPost(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return postService.getPost(id, userDetails.getUser());
+    public ResponseEntity<PostResponseDto> getPost(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return ResponseEntity.ok().body(postService.getPost(id, userDetails.getUser()));
     }
 
     @PostMapping("/api/post")
--- src/main/java/pbl/magazine/security/WebSecurityConfig.java
+++ src/main/java/pbl/magazine/security/WebSecurityConfig.java
@@ -5,14 +5,24 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import pbl.magazine.security.filter.FormLoginFilter;
+import pbl.magazine.security.filter.JwtAuthFilter;
+import pbl.magazine.security.jwt.HeaderTokenExtractor;
+import pbl.magazine.security.provider.FormLoginAuthProvider;
+import pbl.magazine.security.provider.JWTAuthProvider;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,15 +31,26 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 @RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final LoginFailHandler loginFailHandler;
     private final List<String> corsList = new ArrayList<>();
+    private final JWTAuthProvider jwtAuthProvider;
+    private final HeaderTokenExtractor headerTokenExtractor;
+
 
     @Bean
     public BCryptPasswordEncoder encodePassword() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) {
+        auth
+                .authenticationProvider(formLoginAuthProvider())
+                .authenticationProvider(jwtAuthProvider);
     }
 
     @Override
@@ -56,6 +77,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable()
         .cors().configurationSource(corsConfigurationSource());
 
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http
+                .addFilterBefore(formLoginFilter(), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
 
         http.authorizeRequests()
                 .antMatchers("/images/**").permitAll()
@@ -76,6 +104,68 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .logout()
                     .permitAll();
     }
+
+    @Bean
+    public FormLoginFilter formLoginFilter() throws Exception {
+        FormLoginFilter formLoginFilter = new FormLoginFilter(authenticationManager());
+        formLoginFilter.setFilterProcessesUrl("/user/login");
+        formLoginFilter.setAuthenticationSuccessHandler(formLoginSuccessHandler());
+        formLoginFilter.afterPropertiesSet();
+        return formLoginFilter;
+    }
+
+    @Bean
+    public FormLoginSuccessHandler formLoginSuccessHandler() {
+        return new FormLoginSuccessHandler();
+    }
+
+    @Bean
+    public FormLoginAuthProvider formLoginAuthProvider() {
+        return new FormLoginAuthProvider(encodePassword());
+    }
+
+    private JwtAuthFilter jwtFilter() throws Exception {
+        List<String> skipPathList = new ArrayList<>();
+
+        // Static 정보 접근 허용
+        skipPathList.add("GET,/images/**");
+        skipPathList.add("GET,/css/**");
+
+        // h2-console 허용
+        skipPathList.add("GET,/h2-console/**");
+        skipPathList.add("POST,/h2-console/**");
+        // 회원 관리 API 허용
+        skipPathList.add("GET,/api/login");
+        skipPathList.add("GET,/api/signup");
+        skipPathList.add("GET,/api/post");
+        skipPathList.add("PUT,/api/post/{id}/like");
+        skipPathList.add("POST,/api/signup");
+
+        skipPathList.add("GET,/");
+        skipPathList.add("GET,/basic.js");
+
+        skipPathList.add("GET,/favicon.ico");
+
+        FilterSkipMatcher matcher = new FilterSkipMatcher(
+                skipPathList,
+                "/**"
+        );
+
+        JwtAuthFilter filter = new JwtAuthFilter(
+                matcher,
+                headerTokenExtractor
+        );
+        filter.setAuthenticationManager(super.authenticationManagerBean());
+
+        return filter;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
 }
 
 
